ea1941ee3df60374aeec10d7269729876f04c53b
meteor1993 <inwsy@hotmail.com>
2020-04-20 07:16:26 +0000

    Deploying to gh-pages from  @ 00a5ba470b145d70c989b68a3bf57b9ab41d2ae2 🚀

---
+++ precache-manifest.8dea5719285f0f4c573c5fe04b0ef499.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5e17e9004fecefb95d127c992625d3e",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "b00cf9913e471f7242e2",
+    "url": "/github-actions-demo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "dbe06d77358ccbee8c8c",
+    "url": "/github-actions-demo/static/js/2.53fb50e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-actions-demo/static/js/2.53fb50e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b00cf9913e471f7242e2",
+    "url": "/github-actions-demo/static/js/main.59cfae84.chunk.js"
+  },
+  {
+    "revision": "096962ac23866891b8e6",
+    "url": "/github-actions-demo/static/js/runtime-main.ca6d70dd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-actions-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
